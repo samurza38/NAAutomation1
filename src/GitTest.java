2db6318cafb599250918b9dee127056acb5c5cbf
samurza38 <smurza9@gmail.com>
2019-10-08 17:24:27 -0600

    13.th commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("12");
         System.out.println("14");
+        System.out.println("17");
  
     }
 }
