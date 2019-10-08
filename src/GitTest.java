074834e10b71a41e119d9e76bacafbd4fe48b4bc
samurza38 <54605590+samurza38@users.noreply.github.com>
2019-10-08 17:23:20 -0600

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,7 +2,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("12");
         System.out.println("14");
-        System.out.println("15");
-        System.out.println("16");
+ 
     }
 }
