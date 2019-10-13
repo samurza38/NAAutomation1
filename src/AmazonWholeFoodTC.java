9764845e7ae0f035fe6b0207aa155c8b8fb6aa6a
samurza38 <smurza9@gmail.com>
2019-10-13 12:15:28 -0600

    22.th commit

--- src/AmazonWholeFoodTC.java
+++ src/AmazonWholeFoodTC.java
@@ -1,2 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class AmazonWholeFoodTC {
+    public static void main(String[] args) throws InterruptedException {
+        Browser myBrowser =new Browser();
+        WebDriver driver =myBrowser.invokeChromeBrowser();
+        driver.get("http://www.amazon.com");
+        driver.findElement(By.xpath("//a[contains(text(),'Whole Foods')]")).click();
+        driver.findElement(By.cssSelector("input#twotabsearchtextbox")).sendKeys("shoes");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("input.nav-input")).click();
+    }
 }
