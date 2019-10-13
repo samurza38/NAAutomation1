4ebdf4429903d4a5c3cd6e37457ba60d5b0c0792
samurza38 <smurza9@gmail.com>
2019-10-12 23:19:16 -0600

    18.th commit

--- src/TryCsssSelectorOnFacebook.java
+++ src/TryCsssSelectorOnFacebook.java
@@ -2,10 +2,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class TryCsssSelectorOnFacebook {
-    public static void main(String[]args){
+    public static void main(String[]args) throws InterruptedException {
         Browser myBrowser=new Browser();
         WebDriver driver=myBrowser.invokeChromeBrowser();
         driver.get("http://www.facebook.com");
         driver.findElement(By.cssSelector("input#email")).sendKeys("sami@gmail.com");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("pww");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input#email")).clear();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input#pass")).clear();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Forgot account?']")).click();
     }
 }
