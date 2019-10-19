64d7aedf64ce09985b89037a2896958a02dc4708
samurza38 <smurza9@gmail.com>
2019-10-18 23:23:02 -0600

    27 th commit

---
+++ src/CheckBox.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckBox {
+    public static void main(String[] args) throws InterruptedException {
+        Browser myBrowser=new Browser();
+        WebDriver driver=myBrowser.invokeChromeBrowser();
+        driver.get("http://www.expedia.com");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("input#partialHotelBooking-hp-package")).click();
+        Thread.sleep(2000);
+        System.out.println("is"+driver.findElement(By.cssSelector("input#partialHotelBooking-hp-package")).isSelected());
+        Thread.sleep(2000);
+        Thread.sleep(2000);
+
+    }
+}
